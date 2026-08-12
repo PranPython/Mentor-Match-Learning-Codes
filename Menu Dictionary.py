bb198d16edfac8ea55579c0db215932117df1e2b
PranPython <pranav.paulraj29@gmail.com>
2026-08-12 10:40:53 +0400

    Update Menu Dictionary.py

--- Menu Dictionary.py
+++ Menu Dictionary.py
@@ -5,6 +5,8 @@ c=t.Screen()
 c.bgcolor("Black")
 pen.color("Red")
 ans = t.Turtle()
+ans.penup()
+ans.color("Red")
 pen.penup()
 pen.goto(-70,350)
 pen.write("---Menu---",font=("Arial",30,"bold"))
@@ -22,9 +24,10 @@ pen.goto(0,0)
 pen.write("Select an option",font=("Arial",25,"bold"),align="center")
 d = {}
 def quest():
-    t.numinput("Enter","Enter the number of what you want to do(1-5)")
+    return t.numinput("Enter","Enter the number of what you want to do(1-5)")
 while True:
-    if quest() == 1:
+    choice = quest()
+    if choice == 1:
         w = t.textinput("Enter","Enter the Word")
         meaning = t.textinput("Enter","Enter The Meaning")
         d[w]=meaning
@@ -32,11 +35,31 @@ while True:
         ans.write(("The word has been added"),font=("Arial",25,"bold"),align="center")
         time.sleep(3)
         ans.clear()
-        quest()
-    elif quest() == 2:
+    elif choice == 2:
+        y = -40
         for i,j in d.items():
-            pen.write((i,j),font=("Arial",25,"bold"),align="center")
-    elif quest() == 5:
+            ans.goto(0,y)
+            ans.write((i,j),font=("Arial",25,"bold"),align="center")
+            time.sleep(5)
+            ans.clear()
+            y = y-40
+    elif choice == 3:
+        z = t.textinput("Enter","What word do you want to view")
+        if z in d:
+            ans.write((z,d[z]),font=("Arial",25,"bold"),align="center")
+            time.sleep(3)
+            ans.clear()
+        else:
+            ans.write("The word you searched is not in the dictionary",font=("Arial",25,"bold"),align="center")
+            time.sleep(3)
+            ans.clear()
+    elif choice == 4:
+        b = t.textinput("Enter","What word do you want to delete?")
+        del d[b]
+        ans.write(("The word has been deleted"),font=("Arial",25,"bold"),align="center")
+        time.sleep(3)
+        ans.clear()
+    elif choice == 5:
         break
 
 
