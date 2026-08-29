71464eecd4827c246d36636da68ae8db302392bf
PranPython <pranav.paulraj29@gmail.com>
2026-08-29 10:59:23 +0400

    Update game.py

--- game.py
+++ game.py
@@ -6,13 +6,13 @@ HEIGHT = 600
 
 ship = Actor("ship")
 ship.pos = (400, 550)
-
+beam = Actor("beam")
 rock = Actor("meteor")
 rock.pos = (random.randint(50, 750), 0)
 
 score = 0
 game_over = False
-
+bullet_active = False
 
 
 def draw():
@@ -21,14 +21,15 @@ def draw():
 
     ship.draw()
     rock.draw()
-
+    if bullet_active == True:
+        beam.draw()
     screen.draw.text("Score: " + str(score), (20, 20), fontsize=35)
 
     if game_over:
         screen.draw.text("GAME OVER", center=(400, 300), fontsize=60)
 
 def update():
-    global score, game_over
+    global score, game_over, bullet_active
 
     if game_over:
         return
@@ -39,12 +40,16 @@ def update():
     if keyboard.right:
         ship.x += 5
 
+    if bullet_active == True:
+        beam.y -= 5
+        if beam.bottom < 0:
+            bullet_active = False
+
     rock.y += 5
 
     if rock.y > 600:
         rock.y = 0
         rock.x = random.randint(50, 750)
-        score += 1
 
     if ship.colliderect(rock):
         game_over = True
@@ -58,5 +63,21 @@ def update():
     if ship.bottom > HEIGHT:
         ship.bottom = HEIGHT
 
+    if beam.colliderect(rock):
+        rock.y = 0
+        rock.x = random.randint(50, 750)
+        score += 1
+        bullet_active = False
 
+        
+
+
+def on_key_down(key):
+    global bullet_active
+    if key == keys.SPACE:
+        if not bullet_active:
+            beam.pos = (ship.x,ship.top)
+            bullet_active = True
+
+    
 pgzrun.go()
